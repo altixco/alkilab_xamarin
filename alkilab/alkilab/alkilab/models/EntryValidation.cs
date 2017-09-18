36ae33a297e8ebbf2d870a83253ba6713f03796e
icnovaro@gmail.com <icnovaro@gmail.com>
2017-09-18 09:21:30 -0500

    adds the form validation

---
+++ alkilab/alkilab/alkilab/models/EntryValidation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alkilab.models
+{
+    public class EntryValidation
+    {
+        public bool isValid { get; set; }
+        public string message { get; set; }
+    }
+}
